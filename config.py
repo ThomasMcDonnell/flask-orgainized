ac194fabf20e6522e651a46231fec28e9216725e
Thomas <mcdonnellthomas89@gmail.com>
2018-08-06 20:39:43 +0100

    fixed database_url setting

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
             'sqlite:///' + os.path.join(basedir, 'todo.db')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
